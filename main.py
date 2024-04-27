04447ed3231b7bf865f30b80d058cd2c91610a11
gustav <gustav.hisker@outlook.de>
2024-04-27 14:26:51 +0200

    finished read in

--- main.py
+++ main.py
@@ -8,25 +8,34 @@ class Bank:
 
 
 def readFileData(path):
-    f = open(path)
-    m, b, d = map(int, f.readline().strip().split(" "))
-    W = {}
-    for i, w in enumerate(map(int, f.readline().strip().split(" "))):
-        W[i] = w
+    with open(path) as f:
+        m, b, d = map(int, f.readline().strip().split(" "))
+        W = {}
+        for i, w in enumerate(map(int, f.readline().strip().split(" "))):
+            W[i] = w
 
-    B = set()
-    mode = False
-    cur = None
-    i = 0
-    for line in f:
-        line = line.strip()
-        if mode:  #
-            m, r, p = map(int, line.split(" "))
-            cur = Bank(r, p, i)
-        else:  # Münzen
-            cur.M = set(map(int, line.split(" ")))
-            B.add(cur)
+        B = set()
+        mode = False
+        cur = None
+        i = 0
+        for line in f:
+            line = line.strip()
+            if mode:  #
+                m, r, p = map(int, line.split(" "))
+                cur = Bank(r, p, i)
+            else:  # Münzen
+                cur.M = set(map(int, line.split(" ")))
+                B.add(cur)
 
-        mode = not mode
-        i += 1
+            mode = not mode
+            i += 1
 
+        return m, b, d, W, B
+
+
+def greedy(path):
+    m, b, d, W, B = readFileData(path)
+
+    aScores = {}
+
+    for
